e2568baddbaa31925d79e6484e6e1309c7e86dfc
wisdomtooth <cbtsao47@gmail.com>
2018-12-19 22:35:48 -0600

    add navbar animation

--- copy/script.js
+++ copy/script.js
@@ -1,9 +1,18 @@
 const hamburger = document.querySelector(".menu__hamburger");
 const navBar = document.querySelector(".menu__open");
-console.log(hamburger);
+const bars = document.querySelectorAll(".menu__hamburger--bar");
+// const topBar = document.querySelector(".menu__hamburger--bar:nth-child(1)");
+// const midBar = document.querySelector(".menu__hamburger--bar:nth-child(2)");
+// const botBar = document.querySelector(".menu__hamburger--bar:nth-child(3)");
 
 function openNavBar(event) {
   navBar.classList.toggle("toggle");
+  bars.forEach(function(bar) {
+    bar.classList.toggle("menu__hamburger--toggle");
+  });
+  // topBar.classList.toggle("menu__hamburger--toggle");
+  // midBar.classList.toggle("menu__hamburger--toggle");
+  // botBar.classList.toggle("menu__hamburger--toggle");
 }
 
 hamburger.addEventListener("click", openNavBar);
